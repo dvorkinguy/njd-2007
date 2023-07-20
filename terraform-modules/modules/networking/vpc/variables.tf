e13a5bdd570e083de577babefcbc4737d1880aad
Guy Dvorkin <dvorkin.guy@gmail.com>
2023-07-20 19:44:03 +0300

    0.1.4

--- terraform-modules/modules/networking/vpc/variables.tf
+++ terraform-modules/modules/networking/vpc/variables.tf
@@ -6,7 +6,7 @@ variable "deployment_prefix" {
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden."
   type        = string
-  default     = "0.0.0.0"
+  default     = "0.0.0.0/16"
 }
 
 variable "single_nat_gateway" {
